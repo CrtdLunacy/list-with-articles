d133f9f79e982eb69d725a20423c5b978c778993
Vyacheslav Frolov <crtdlunacy@gmail.com>
2023-03-02 12:35:53 +0400

    fix fonts

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,18 @@ import {sleep} from "@/helpers/sleep";
 import ArticlesWidget from "@/components/ArticlesWidget/ArticlesWidget";
 import Image from "next/image";
 import Dots from "@/public/dots.svg";
+import { Montserrat } from 'next/font/google';
 
+const montserrat = Montserrat({
+  subsets: ['cyrillic'],
+  weight: ['400', '700'],
+})
 
 export default async function Home() {
   await sleep(1000);
 
   return (
-    <main className={styles.main}>
+    <main className={`${styles.main} ${montserrat.className}`}>
       <Image className={styles.dots} src={Dots} alt='dotsBachground'/>
       <ArticlesWidget data={file.data as MediaGroupsModel[]} />
     </main>
